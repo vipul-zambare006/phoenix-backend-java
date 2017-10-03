408b5f89396dc71e3d0eb06b88fd413547d9fa56
RachelGnanaraj <e0146843@u.nus.edu>
2017-10-03 23:10:33 +0800

    Test cases

--- PRMS/test/sg/edu/nus/iss/phoenix/schedule/service/test/ProgramSlotDaoTest.java
+++ PRMS/test/sg/edu/nus/iss/phoenix/schedule/service/test/ProgramSlotDaoTest.java
@@ -7,6 +7,9 @@ package sg.edu.nus.iss.phoenix.schedule.service.test;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
+import junit.framework.TestCase;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.mockito.Mock;
@@ -18,48 +21,56 @@ import sg.edu.nus.iss.phoenix.core.exceptions.NotFoundException;
 import sg.edu.nus.iss.phoenix.schedule.entity.ProgramSlot;
 import sg.edu.nus.iss.phoenix.schedule.dao.ProgramSlotDAO;
 
+public class ProgramSlotDaoTest extends TestCase {
 
-public class ProgramSlotDaoTest {
-    
     private static ProgramSlot programSlot;
     private static ArrayList<ProgramSlot> programSlotList = new ArrayList<>();
-    
+
     @Mock
     private static ProgramSlotDAO dao;
-    
-    public ProgramSlotDaoTest(){
+
+    public ProgramSlotDaoTest() {
     }
-    
-    @BeforeClass
-    public static void setup() throws SQLException{
-    DAOFactoryImpl daoImpl =  mock(DAOFactoryImpl.class);
+
+    @Before
+    public static void setup() throws SQLException {
+        DAOFactoryImpl daoImpl = mock(DAOFactoryImpl.class);
         dao = mock(ProgramSlotDAO.class);
         when(daoImpl.getProgramSlotDAO()).thenReturn(dao);
+        programSlot = new ProgramSlot("60", "2017-09-09", "pointyhead", "13:00:00", "dance floor", "dilbert", "wally");
+        when(dao.loadAll()).thenReturn((List<ProgramSlot>) programSlot);
         programSlotList.add(new ProgramSlot("60", "2017-09-09", "pointyhead", "13:00:00", "dance floor", "dilbert", "wally"));
         programSlotList.add(new ProgramSlot("90", "2017-09-12", "pointyhead", "12:00:00", "newz", "kilbert", "wally"));
         programSlotList.add(new ProgramSlot("15", "2017-09-15", "pointyhead", "15:00:00", "pop songs", "wally", "wally"));
         programSlotList.add(new ProgramSlot("30", "2017-09-20", "pointyhead", "08:00:00", "ad-break", "dilbert", "wally"));
     }
-    
-    @Test(expected=Exception.class)
-    public void testLoadAll() throws SQLException{
-    
+
+    @Test(expected = SQLException.class)
+    public void testCreate() throws SQLException {
+        DAOFactoryImpl daoImpl = mock(DAOFactoryImpl.class);
+        dao = mock(ProgramSlotDAO.class);
+        when(daoImpl.getProgramSlotDAO()).thenReturn(dao);
+        ProgramSlot ps = new ProgramSlot("60", "2017-09-09", "pointyhead", "13:00:00", "dance floor", "dilbert", "wally");
+        Mockito.doThrow(new SQLException()).doNothing().when(dao).create(ps);
+
     }
-    
-    @Test(expected=Exception.class)
-    public void testCreate() throws SQLException{
-        Mockito.doThrow(new Exception()).doNothing().when(dao).create(programSlot);
-        
+
+    @Test(expected = SQLException.class)
+    public void testSave() throws SQLException, NotFoundException {
+        DAOFactoryImpl daoImpl = mock(DAOFactoryImpl.class);
+        dao = mock(ProgramSlotDAO.class);
+        when(daoImpl.getProgramSlotDAO()).thenReturn(dao);
+        ProgramSlot ps = new ProgramSlot("60", "2017-09-09", "pointyhead", "13:00:00", "dance floor", "dilbert", "wally");
+        Mockito.doThrow(new SQLException()).doNothing().when(dao).save(programSlot);
     }
-    
-    @Test(expected=Exception.class)
-    public void testSave() throws SQLException, NotFoundException{
-        Mockito.doThrow(new Exception()).doNothing().when(dao).save(programSlot);
+
+    @Test(expected = SQLException.class)
+    public void testDelete() throws SQLException, NotFoundException {
+        DAOFactoryImpl daoImpl = mock(DAOFactoryImpl.class);
+        dao = mock(ProgramSlotDAO.class);
+        when(daoImpl.getProgramSlotDAO()).thenReturn(dao);
+        ProgramSlot ps = new ProgramSlot("60", "2017-09-09", "pointyhead", "13:00:00", "dance floor", "dilbert", "wally");
+        Mockito.doThrow(new SQLException()).doNothing().when(dao).delete(programSlot);
     }
-    
-     @Test(expected=Exception.class)
-    public void testDelete() throws SQLException, NotFoundException{
-        Mockito.doThrow(new Exception()).doNothing().when(dao).delete(programSlot);
-    }
-    
+
 }
--- PRMS/test/sg/edu/nus/iss/phoenix/user/service/test/AllUserTests.java
+++ PRMS/test/sg/edu/nus/iss/phoenix/user/service/test/AllUserTests.java
@@ -5,7 +5,6 @@
  */
 package sg.edu.nus.iss.phoenix.user.service.test;
 
-import junit.framework.TestSuite;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 /**
@@ -18,6 +17,7 @@ import org.junit.runners.Suite;
   UserDaoTest.class,
   UserServiceTest.class,
   UserTest.class,
+  
 })
 
 public class AllUserTests {
